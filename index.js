65435f65658757d5d88e365dc5fb1ca1b9e439dd
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2023-11-19 20:58:24 +0600

    Update index.js

--- index.js
+++ index.js
@@ -91,6 +91,12 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/menu", verifyToken, verifyAdmin, async (req, res) => {
+      const item = req.body;
+      const result = await menuCollection.insertOne(item);
+      res.send(result);
+    });
+
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray();
       res.send(result);
