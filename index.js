0e055318dd6845b7a20e5a06f0a43df81eaad347
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2023-11-22 20:52:36 +0600

    Update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 var jwt = require("jsonwebtoken");
 const port = process.env.PORT || 3300;
 const app = express();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // Middlewaer
 app.use(express.json());
@@ -192,6 +193,23 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await userCollection.deleteOne(query);
       res.send(result);
+    });
+
+    // payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      console.log(amount, "amount inside the intent");
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
     });
 
     await client.db("admin").command({ ping: 1 });
